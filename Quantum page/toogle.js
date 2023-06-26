77e888347c5c8f4e46835ae0e2862c17b7afa7a5
Nikhil8450 <nikhil.palkar@zeuslearning.com>
2023-06-26 10:59:05 +0530

    page change

--- Quantum page/toogle.js
+++ Quantum page/toogle.js
@@ -1,5 +1,11 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const menuContent = document.querySelector(".menu-content");
-hamburgerMenu.addEventListener("click", function () {
-menuContent.classList.toggle("active");
+// DOM elements
+var hamburgerMenu = document.querySelector('.hamburger-menu');
+var hamburgerIcon = document.querySelector('.menu-content');
+// Open hamburger menu on hover
+hamburgerIcon.addEventListener('mouseenter', function () {
+    hamburgerMenu.classList.add('open');
+});
+// Close hamburger menu on mouseout
+hamburgerMenu.addEventListener('mouseleave', function () {
+    hamburgerMenu.classList.remove('open');
 });
